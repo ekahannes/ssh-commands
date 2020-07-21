a547757fef5e4782cf05987ae1434e1851a007a1
Johannes Unruh <johannes@unruh.ai>
2020-07-21 11:18:37 +0200

    asdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,11 @@
 mkdir -p ~/.ssh/
 echo "$1" > ~/.ssh/private.key
 chmod 600 ~/.ssh/private.key
-echo "164.90.177.64 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKtbilgQyw1CB8EefUCqjBzlwp/0+KnTHZfyaJ8xGWU/IPjwrKGyDedtwuUEm3XPqMV4e4Nwa9M3OXihGgRHWfg=" > ~/.ssh/known_hosts
+echo "$2" > ~/.ssh/known_hosts
 
-ls ~/.ssh/
-pwd
-echo "done"
+echo "ssh-keygen"
 ssh-keygen -y -e -f ~/.ssh/private.key
+echo "ssh-keyscan"
 ssh-keyscan 164.90.177.64
 
 ssh -i ~/.ssh/private.key -tt johannes@164.90.177.64 "echo test > testfile1"
