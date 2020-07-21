118dc48a90e67d4db375731d2ab4e97e441ed8f2
Johannes Unruh <johannes@unruh.ai>
2020-07-21 09:58:06 +0200

    asdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,9 @@ mkdir -p ~/.ssh/
 echo "$1" > ../private.key
 chmod 600 ../private.key
 echo "$2" > ~/.ssh/known_hosts
-# cat ~/.ssh/known_hosts
-# cat ../private.key
 
-# echo $1 > ssh_script.sh
+echo "$3" > ssh_script.sh
 
-ssh -i ../private.key -o "StrictHostKeyChecking no" -tt johannes@164.90.177.64 "echo test > testfile1"
-# ssh -i ../private.key -o "StrictHostKeyChecking no" -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
+# ssh -i ../private.key -tt johannes@164.90.177.64 "echo test > testfile1"
+ssh -i ../private.key -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
 # cat ../private.key
