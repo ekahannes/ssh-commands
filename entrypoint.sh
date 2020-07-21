44b8069a07ec19a2dcce4092e1e0681058ade6a3
Johannes Unruh <johannes@unruh.ai>
2020-07-21 03:40:09 +0200

    asdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 mkdir -p ~/.ssh/
 echo $SSH_PRIVATE_KEY > ../private.key
 chmod 600 ../private.key
-echo "164.90.177.64 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKtbilgQyw1CB8EefUCqjBzlwp/0+KnTHZfyaJ8xGWU/IPjwrKGyDedtwuUEm3XPqMV4e4Nwa9M3OXihGgRHWfg=" > ~/.ssh/known_hosts
+echo $SSH_KNOWN_HOSTS > ~/.ssh/known_hosts
+cat ~/.ssh/known_hosts
 
 echo $1 > ssh_script.sh
 
