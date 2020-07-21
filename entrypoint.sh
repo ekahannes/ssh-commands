aeafde697ed77e55d00076735a041d953ada4133
Johannes Unruh <johannes@unruh.ai>
2020-07-21 10:32:33 +0200

    asdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ pwd
 echo "done"
 ssh-keygen -y -e -f ~/.ssh/private.key
 
-ssh -i ~/.ssh/private.key -tt johannes@164.90.177.64 "echo test > testfile1"
+ssh -i ~/.ssh/private.key -o "StrictHostKeyChecking no" -tt johannes@164.90.177.64 "echo test > testfile1"
 # ssh -i ../private.key -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
 # cat ../private.key
