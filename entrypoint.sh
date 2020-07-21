92adbf60d32985e54cdd2d71c025ed21a6d853c5
Johannes Unruh <johannes@unruh.ai>
2020-07-21 11:08:24 +0200

    asdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ echo "done"
 ssh-keygen -y -e -f ~/.ssh/private.key
 ssh-keyscan 164.90.177.64
 
-ssh -i ~/.ssh/private.key -tt johannes@164.90.177.64 "echo test > testfile1"
+ssh -i ~/.ssh/private.key -o UserKnownHostsFile=~/.ssh/known_hosts -tt johannes@164.90.177.64 "echo test > testfile1"
 echo "known hosts file:"
 cat ~/.ssh/known_hosts
 # ssh -i ../private.key -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
