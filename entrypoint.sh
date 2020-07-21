597e3c7c4d791a334ca9cc278c0fbcf6a4655f1b
Johannes Unruh <johannes@unruh.ai>
2020-07-21 03:13:29 +0200

    asdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 printenv
 mkdir -p ~/.ssh/
 echo $SSH_PRIVATE_KEY > ../private.key
-sudo chmod 600 ../private.key
+chmod 600 ../private.key
 echo $SSH_KNOWN_HOSTS_DEV > ~/.ssh/known_hosts
 
 echo $1 > ssh_script.sh
 
-ssh -i ~/.ssh/id_rsa.johannes_ekaros -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
+ssh -i ../private.key -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
