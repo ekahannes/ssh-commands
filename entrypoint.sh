d4767829576c0d786a220af2f5bbb4767c8d4a8b
Johannes Unruh <johannes@unruh.ai>
2020-07-21 10:28:35 +0200

    asdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ chmod 600 ~/.ssh/private.key
 echo "$2" > ~/.ssh/known_hosts
 
 ls ~/.ssh/
-ls ../
 pwd
+echo "done"
 
 ssh -i ~/.ssh/private.key -tt johannes@164.90.177.64 "echo test > testfile1"
 # ssh -i ../private.key -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
