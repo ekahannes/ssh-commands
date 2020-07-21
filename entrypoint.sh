e79f0c08c2115444f9f88f69c98420f9fefdea31
Johannes Unruh <johannes@unruh.ai>
2020-07-21 12:06:02 +0200

    asdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,5 +13,6 @@ ssh-keyscan 164.90.177.64
 ssh -i ~/.ssh/private.key -tt johannes@164.90.177.64 "echo test > testfile1"
 echo "known hosts"
 cat ~/.ssh/known_hosts
+wc -m ~/.ssh/known_hosts
 # ssh -i ../private.key  -o "StrictHostKeyChecking no" -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
 # cat ../private.key
