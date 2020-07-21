e42b8015f90249258640d48022eae2e711539a12
Johannes Unruh <johannes@unruh.ai>
2020-07-21 12:01:24 +0200

    asdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-apt update && apt install openssh-client -y
-
 mkdir -p ~/.ssh/
 echo "$1" > ~/.ssh/private.key
 chmod 600 ~/.ssh/private.key
@@ -15,5 +13,5 @@ ssh-keyscan 164.90.177.64
 ssh -i ~/.ssh/private.key -tt johannes@164.90.177.64 "echo test > testfile1"
 echo "known hosts"
 cat ~/.ssh/known_hosts
-# ssh -i ../private.key -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
+# ssh -i ../private.key  -o "StrictHostKeyChecking no" -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
 # cat ../private.key
