e96e9492d9228471811df7ef90e3cb5d147d4723
Johannes Unruh <johannes@unruh.ai>
2020-07-22 16:57:38 +0200

    final github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ chmod +x ssh_script.sh
 
 # jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4"
 # printenv
-$( echo "$4" | jq -r 'keys[] as $k | "export \($k)=\(.[$k])"' )
+$(jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4")
 
 ssh -i ~/.ssh/private.key -o UserKnownHostsFile=$HOME/.ssh/known_hosts -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh "$4"
 # ssh -i ../private.key  -o "StrictHostKeyChecking no" -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
