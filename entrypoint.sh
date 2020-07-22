c562262da8bd6a2d58fe5ea9c18599b40d51131f
Johannes Unruh <johannes@unruh.ai>
2020-07-22 10:54:02 +0200

    asdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,8 @@ mkdir -p ~/.ssh/
 echo "$1" > ~/.ssh/private.key
 chmod 600 ~/.ssh/private.key
 echo "$2" > ~/.ssh/known_hosts
-chmod 600 ~/.ssh/known_hosts
 
-echo "ssh-keygen"
-ssh-keygen -y -e -f ~/.ssh/private.key
-echo "ssh-keyscan"
-ssh-keyscan 164.90.177.64
+
 
 ssh -i ~/.ssh/private.key -o UserKnownHostsFile=$HOME/.ssh/known_hosts -tt johannes@164.90.177.64 "echo test > testfile1"
-echo $HOME
-# cat ~/.ssh/known_hosts
-# wc -m ~/.ssh/known_hosts
-
 # ssh -i ../private.key  -o "StrictHostKeyChecking no" -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
-# cat ../private.key
