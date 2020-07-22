422630464c49439f2dca8abab4ab87db1f8f4806
Johannes Unruh <johannes@unruh.ai>
2020-07-22 17:53:04 +0200

    final github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,6 @@ echo "$1" > ~/.ssh/private.key
 chmod 600 ~/.ssh/private.key
 echo "$2" > ~/.ssh/known_hosts
 
-echo "${3}exit" > ssh_script.sh
-chmod +x ssh_script.sh
-
 # jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4"
 # printenv
 # jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4"
@@ -33,5 +30,8 @@ echo $STR_ARGS
 # done
 # echo ${myarray[ARG]}
 
-ssh -i ~/.ssh/private.key -o UserKnownHostsFile=$HOME/.ssh/known_hosts -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh "$(STR_ARGS)"
+echo "$STR_ARGS;${3}exit" > ssh_script.sh
+chmod +x ssh_script.sh
+
+ssh -i ~/.ssh/private.key -o UserKnownHostsFile=$HOME/.ssh/known_hosts -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh
 # ssh -i ../private.key  -o "StrictHostKeyChecking no" -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
