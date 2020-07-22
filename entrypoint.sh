b163731fdd7bfccf2ebcdd71af5448a015348332
Johannes Unruh <johannes@unruh.ai>
2020-07-22 18:16:25 +0200

    final github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,15 @@ echo "$2" > ~/.ssh/known_hosts
 # jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4"
 
 STR_ARGS="export "
+STR_UNSET="unset"
 for s in $(jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4"); do
     # export $s
     STR_ARGS="${STR_ARGS} $s"
+    IFS='=' read -r key val <<< "$s"
+    STR_UNSET="${STR_UNSET} $key"
 done
 echo $STR_ARGS
+echo $STR_UNSET
 
 # declare -A myarray
 # while IFS="=" read -r key value
@@ -30,7 +34,7 @@ echo $STR_ARGS
 # done
 # echo ${myarray[ARG]}
 
-echo "eval $STR_ARGS; printenv; ${3}exit" > ssh_script.sh
+echo "eval $STR_ARGS;${3}exit; eval" > ssh_script.sh
 chmod +x ssh_script.sh
 # ssh -i ~/.ssh/private.key -o UserKnownHostsFile=$HOME/.ssh/known_hosts -tt johannes@164.90.177.64 "eval $STR_ARGS"
 ssh -i ~/.ssh/private.key -o UserKnownHostsFile=$HOME/.ssh/known_hosts -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh test
