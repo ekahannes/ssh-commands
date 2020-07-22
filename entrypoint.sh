eb86d77f4ad2ff6d95cb8fa5431ecf73e23ebfc6
Johannes Unruh <johannes@unruh.ai>
2020-07-22 17:13:08 +0200

    final github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,22 @@ chmod +x ssh_script.sh
 # jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4"
 # printenv
 # jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4"
-for s in $(jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4"); do
-    export $s
-done
+# for s in $(jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4"); do
+#     export $s
+# done
 
-printenv
+declare -A myarray
+while IFS="=" read -r key value
+do
+    myarray[$key]="$value"
+done < <(jq -r 'to_entries|map("(.key)=(.value)")|.[]' <<< "$4")
+
+echo myarray
+
+for key in "${!myarray[@]}"
+do
+    echo "$key = ${myarray[$key]}"
+done
 
 ssh -i ~/.ssh/private.key -o UserKnownHostsFile=$HOME/.ssh/known_hosts -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh
 # ssh -i ../private.key  -o "StrictHostKeyChecking no" -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
