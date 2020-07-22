5d539a50a5b2e492648e04a523143485932fb815
Johannes Unruh <johannes@unruh.ai>
2020-07-22 19:56:41 +0200

    replaced $home with pwd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # setup files required for ssh
-mkdir -p /github/workspace/.ssh/
-echo "$1" > /github/workspace/.ssh/private.key
-chmod 600 /github/workspace/.ssh/private.key
-echo "$2" > /github/workspace/.ssh/known_hosts
+mkdir -p /github/home/.ssh/
+echo "$1" > /github/home/.ssh/private.key
+chmod 600 /github/home/.ssh/private.key
+echo "$2" > /github/home/.ssh/known_hosts
 
 # setup export of variables to remote environment
 STR_ARGS="export"
@@ -19,6 +19,6 @@ done
 # write commands script to local file
 echo "eval $STR_ARGS;${3}exit; eval $STR_UNSET" > ssh_script.sh
 chmod +x ssh_script.sh
-echo $HOME
+
 # execute commands script on remote via ssh
-ssh -i /github/workspace/.ssh/private.key -o UserKnownHostsFile=/github/workspace/.ssh/known_hosts -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh
+ssh -i /github/home/.ssh/private.key -o UserKnownHostsFile=/github/home/.ssh/known_hosts -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh
