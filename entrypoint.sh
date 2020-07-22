f56feed29582d5e9297feb43ecf410f109b60fb4
Johannes Unruh <johannes@unruh.ai>
2020-07-22 20:33:39 +0200

    added $home again and vars for ip and host

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # setup files required for ssh
-mkdir -p /github/home/.ssh/
-echo "$1" > /github/home/.ssh/private.key
-chmod 600 /github/home/.ssh/private.key
-echo "$2" > /github/home/.ssh/known_hosts
+mkdir -p ~/.ssh/
+echo "$1" > ~/.ssh/private.key
+chmod 600 ~/.ssh/private.key
+echo "$2" > ~/.ssh/known_hosts
 
 # setup export of variables to remote environment
 STR_ARGS="export"
@@ -22,4 +22,4 @@ echo "eval $STR_ARGS;${3}exit; eval $STR_UNSET" > ssh_script.sh
 chmod +x ssh_script.sh
 
 # execute commands script on remote via ssh
-ssh -i /github/home/.ssh/private.key -o UserKnownHostsFile=/github/home/.ssh/known_hosts -tt ${SSH_USERNAME}@${SERVER_IP} 'bash -s' < $(pwd)/ssh_script.sh
+ssh -i ~/.ssh/private.key -o UserKnownHostsFile=$HOME/.ssh/known_hosts -tt ${SSH_USERNAME}@${SERVER_IP} 'bash -s' < $(pwd)/ssh_script.sh
