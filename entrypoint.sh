156f5bb47a5755f802b696a0719617c29defb67c
Johannes Unruh <johannes@unruh.ai>
2020-07-22 16:52:15 +0200

    final github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,9 @@ echo "$2" > ~/.ssh/known_hosts
 echo "${3}exit" > ssh_script.sh
 chmod +x ssh_script.sh
 
+# jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4"
+# printenv
+echo "$4"
+
 ssh -i ~/.ssh/private.key -o UserKnownHostsFile=$HOME/.ssh/known_hosts -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh "$4"
 # ssh -i ../private.key  -o "StrictHostKeyChecking no" -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh arg1
