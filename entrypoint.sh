363f41176bfc5e541220071872d3631b83d93487
Johannes Unruh <johannes@unruh.ai>
2020-07-23 12:02:46 +0200

    surrounded $s with quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ STR_ARGS="export"
 STR_UNSET="unset"
 echo "$4" > file.json
 # UNPRETTY_JSON= $(echo "$4" | jq -c)
-echo "$(jq -r 'to_entries|map("(.key)=(.value)")|.[]' file.json)"
+echo "$(jq -r "to_entries|map(\"(.key)=(.value)\")|.[]" file.json)"
 STRING=$(jq -r "to_entries|map(\"\(.key)=\(.value)\")|.[]" <<< "${4}")
 # for s in $STRING; do
     # export $s
