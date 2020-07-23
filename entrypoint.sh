e4f1cbcff2d74db6d5f3b7f89ed8eceb975d5b81
Johannes Unruh <johannes@unruh.ai>
2020-07-23 13:15:31 +0200

    surrounded $s with quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,12 +12,12 @@ STR_UNSET="unset"
 # echo "$4" file.json
 # UNPRETTY_JSON= $(echo "$4" | jq -c)
 # echo  "$(jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' <<< "${4}")"
-STRING="$(jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' <<< "${4}")"
+STRING="$(jq -r 'to_entries|map("\(.key)=\"\(.value)\"")|.[]' <<< "${4}")"
 # echo $STRING
 while IFS= read -r line; do
-    echo "\"$line\""
-    export "\"$line\""
-    STR_ARGS="${STR_ARGS} \"$line\""
+    echo "$line"
+    export "${line}"
+    STR_ARGS="${STR_ARGS} ${line}"
     # IFS='=' read -r key val <<< "${s}"
     # STR_UNSET="${STR_UNSET} $key"
 done <<< "$STRING"
