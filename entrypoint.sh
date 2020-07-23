730d0c3a9a4aa2924e3505626d5c301e1b767d6e
Johannes Unruh <johannes@unruh.ai>
2020-07-23 12:42:19 +0200

    surrounded $s with quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,19 +12,18 @@ STR_UNSET="unset"
 # echo "$4" file.json
 # UNPRETTY_JSON= $(echo "$4" | jq -c)
 # echo  "$(jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' <<< "${4}")"
-STRING="$(jq -r 'to_entries|map("\(.key)=\(.value)")|.[]|tostring' <<< "${4}")"
+STRING="$(jq -r 'to_entries|map("\(.key)=\(.value)\n")|.[]' <<< "${4}")"
 # echo $STRING
-# while IFS= read -r line; do
-#     echo "... $line ..."
-# done <<< "$STRING"
-for s in $STRING; do
-    export $s
-    echo "${s}"
-    export "${s}"
-    STR_ARGS="${STR_ARGS} ${s}"
-    IFS='=' read -r key val <<< "${s}"
-    STR_UNSET="${STR_UNSET} $key"
-done
+while IFS= read -r line; do
+    echo "... $line ..."
+done <<< "$STRING"
+# for s in $STRING; do
+#     echo "${s}"
+    # export "${s}"
+    # STR_ARGS="${STR_ARGS} ${s}"
+    # IFS='=' read -r key val <<< "${s}"
+    # STR_UNSET="${STR_UNSET} $key"
+# done
 
 # write commands script to local file
 echo "eval $STR_ARGS;${3}exit; eval $STR_UNSET" > ssh_script.sh
