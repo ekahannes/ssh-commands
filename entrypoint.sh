6d27f3713d64752f75f2434f392efcaac2a6d9e4
Johannes Unruh <johannes@unruh.ai>
2020-07-23 13:46:28 +0200

    surrounded $s with quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,30 +9,20 @@ echo "$2" > ~/.ssh/known_hosts
 # setup export of variables to remote environment
 STR_ARGS="export"
 STR_UNSET="unset"
-# echo "$4" file.json
-# UNPRETTY_JSON= $(echo "$4" | jq -c)
-# echo  "$(jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' <<< "${4}")"
-STRING="$(jq -r 'to_entries|map("\(.key)=\"\(.value)\"")|.[]' <<< "${4}")"
-# echo $STRING
-while IFS= read -r line; do
-    echo "$line"
-    export "${line}"
-    STR_ARGS="${STR_ARGS} ${line}"
-    # IFS='=' read -r key val <<< "${s}"
-    # STR_UNSET="${STR_UNSET} $key"
-done <<< "$STRING"
-# for s in $STRING; do
-#     echo "${s}"
-#     export "${s}"
-#     STR_ARGS="${STR_ARGS} ${s}"
-#     IFS='=' read -r key val <<< "${s}"
+$STRING=$(jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4")
+# for s in $(jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$4"); do
+#     export $s
+#     STR_ARGS="${STR_ARGS} $s"
+#     IFS='=' read -r key val <<< "$s"
 #     STR_UNSET="${STR_UNSET} $key"
 # done
-echo $STR_ARGS
+while IFS= read -r line; do
+    echo "... $line ..."
+done <<< "$STRING"
 
 # write commands script to local file
-echo "eval \"$STR_ARGS\";${3}exit; eval $STR_UNSET" > ssh_script.sh
+echo "eval $STR_ARGS;${3}exit; eval $STR_UNSET" > ssh_script.sh
 chmod +x ssh_script.sh
 
 # execute commands script on remote via ssh
-ssh -i ~/.ssh/private.key -o UserKnownHostsFile=$HOME/.ssh/known_hosts -tt johannes@164.90.177.64 'bash -s' < $(pwd)/ssh_script.sh
+ssh -i ~/.ssh/private.key -o UserKnownHostsFile=$HOME/.ssh/known_hosts -tt ${SSH_USERNAME}@${SERVER_IP} 'bash -s' < $(pwd)/ssh_script.sh
